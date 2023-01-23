01278f843223ec17e839b129eb76bc7050dbb88b
RoryV91 <vanblaricom.rory@icloud.com>
2023-01-23 08:57:03 -0800

    back to browserrouter

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {HashRouter as Router} from 'react-router-dom';
+import {BroswerRouter as Router} from 'react-router-dom';
 import App from './App';
 
 
