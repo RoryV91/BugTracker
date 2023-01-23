83423b92c4b77243e558664f27f294ec6a0c2dce
RoryV91 <vanblaricom.rory@icloud.com>
2023-01-23 09:06:17 -0800

    Link added

--- frontend/src/Pages/Login/login.js
+++ frontend/src/Pages/Login/login.js
@@ -1,6 +1,6 @@
 import {useState} from 'react'
 import { loginToAccount } from '../../utils/api';
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, Link } from 'react-router-dom'
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer, toast } from 'react-toastify';
 
